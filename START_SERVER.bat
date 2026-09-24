6c3a835eded775632b3477a9d1480093d9c635d9
TheSolverHub <yaduvanshiboysyt@gmail.com>
2026-09-24 14:00:30 +0530

    Delete START_SERVER.bat

--- START_SERVER.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-set "OWNER_GAME_ID="
-set "ADMIN_TEAM_IDS="
-set "ADMIN_PIN=1234"
-echo Starting Hand Cricket Arena V21...
-echo Admin: Game ID + short Team PIN (Admin Team only)
-npm start
-pause
